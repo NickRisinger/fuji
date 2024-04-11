6167c197f41734eb8f7fa40c0dc48a06bc7c3e65
NickRisinger <boozy044@gmail.com>
2024-04-11 20:37:48 +0300

    chore: update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <>
-      <span className="text-black">page</span>
+      <div className="h-[2000px]">
+        <span className="text-black">page</span>
+      </div>
     </>
   );
 }
