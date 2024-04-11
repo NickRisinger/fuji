9fcef2ca1b6609887fcb85d8ad75911c55324a7a
Boozy044 <boozy044@gmail.com>
2024-04-11 18:23:01 +0300

    chore: update menu

--- src/components/SidebarMenu.tsx
+++ src/components/SidebarMenu.tsx
@@ -1,3 +1,30 @@
-export default function Sidebar() {
+interface MenuItem {
+  title: string;
+  icon: string;
+  'background-color'?: string;
+  'icon-color'?: string;
+  path: string;
+  visible: boolean;
+  mobile?: boolean;
+  subMenu?: MenuItem;
+}
+
+async function getMenuItems() {
+  const res = await fetch(
+    'https://api.crm.staging.arbatdeport.life/v2/access/get-menu-items',
+    {
+      method: 'GET',
+      headers: { 'X-Api-Key': 'IoOFarIAxjFRLgjJSYONgd6Y7gx50epd' },
+    },
+  );
+
+  const data = (await res.json()) as unknown as MenuItem[];
+
+  return data;
+}
+
+export default async function Sidebar() {
+  const menu = await getMenuItems();
+
   return <></>;
 }
