784a555379d3a605332a048911a93a5b8853ce23
Boozy044 <boozy044@gmail.com>
2024-04-20 12:28:26 +0300

    chore: update objects

--- src/app/objects/[id]/edit/page.tsx
+++ src/app/objects/[id]/edit/page.tsx
@@ -1,3 +1,5 @@
+import ObjectForm from '~/components/ObjectForm';
+
 interface IObjectForm {
   id: number;
   title: string;
@@ -30,41 +32,7 @@ export default async function Page({ params }: { params: { id: string } }) {
     <div className="">
       <div className="">Object: {params.id}</div>
       <div className="flex gap-x-5">
-        <div className="flex flex-col">
-          <div className="">
-            <div className="">
-              <input type="text" placeholder="Заголовок" />
-              <input type="text" placeholder="Описание" />
-              <input type="text" placeholder="Адрес" />
-            </div>
-            <div className="flex gap-x-6">
-              <label htmlFor="">
-                <input type="checkbox" placeholder="Описание" />
-                <span>Эксклюзив</span>
-              </label>
-              <label htmlFor="">
-                <input type="checkbox" placeholder="Описание" />
-                <span>Согласие на рекламу</span>
-              </label>
-              <label htmlFor="">
-                <input type="checkbox" placeholder="Описание" />
-                <span>Сопровождение</span>
-              </label>
-            </div>
-          </div>
-          <hr />
-          <div className=""></div>
-          <hr />
-          <div className=""></div>
-          <hr />
-          <div className=""></div>
-          <hr />
-          <div className=""></div>
-          <hr />
-          <div className=""></div>
-          <hr />
-          <div className=""></div>
-        </div>
+        <ObjectForm defaultValues={data} />
         <div className=""></div>
       </div>
     </div>
--- src/components/ObjectForm.tsx
+++ src/components/ObjectForm.tsx
@@ -1,29 +1,143 @@
 'use client';
 
-import { useForm, SubmitHandler } from 'react-hook-form';
+import {
+  FormProvider,
+  useForm,
+  SubmitHandler,
+  useFormContext,
+} from 'react-hook-form';
 
 interface IObjectForm {
   title: string;
+  description: string;
   address: {
     value: string;
   };
+  categoryId: number;
+  operationId: number;
+  price: number;
+  commissionPercent: number;
+  commission: number;
+
+  estateObjectData: {
+    totalArea: number;
+    livingArea: number;
+    kitchenArea: number;
+    roomType: string;
+    apartmentNumber: number;
+    cadastralNumber: string;
+    repairCondition: string;
+    shareAmount: string;
+    balconyType: string;
+    bathroomUnit: string;
+    heatedFloor: string;
+    ceilingHeight: number;
+    viewFromWindow: string;
+    isMortgageAvailable: string;
+  };
 }
 
-export default function ObjectForm() {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm<IObjectForm>();
+export default function ObjectForm({
+  defaultValues,
+}: {
+  defaultValues?: Partial<IObjectForm>;
+}) {
+  const methods = useForm<IObjectForm>({
+    defaultValues,
+  });
 
   const onSubmit: SubmitHandler<IObjectForm> = (data) => console.log(data);
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input defaultValue="test" {...register('title')} />
-      <input defaultValue="test" {...register('title')} />
-      <input defaultValue="test" {...register('address.value')} />
-    </form>
+    <FormProvider {...methods}>
+      <form onSubmit={methods.handleSubmit(onSubmit)} className="w-full">
+        <div className="flex flex-col gap-y-3">
+          <input
+            {...methods.register('title', { required: true, maxLength: 33 })}
+            placeholder="Не указано"
+          />
+          <textarea
+            {...methods.register('description', { required: true })}
+            placeholder="Не указано"
+          />
+          <input
+            {...methods.register('address.value', { required: true })}
+            placeholder="Не указано"
+          />
+        </div>
+
+        <div className="grid grid-cols-3 gap-3">
+          <select
+            {...methods.register('categoryId', {
+              required: true,
+              valueAsNumber: true,
+            })}
+          >
+            <option value="1">Комнаты</option>
+            <option value="2">Квартиры</option>
+            <option value="3">Дома, дачи, коттеджи</option>
+            <option value="4">Коммерческая</option>
+            <option value="5">Земельные участки</option>
+            <option value="6">Гаражи и машиноместа</option>
+            <option value="7">Новостройки</option>
+          </select>
+          <select
+            {...methods.register('operationId', {
+              required: true,
+              valueAsNumber: true,
+            })}
+          >
+            <option value="1">Продажа</option>
+            <option value="2">Аренда</option>
+          </select>
+
+          <input
+            {...methods.register('price', {
+              required: true,
+              valueAsNumber: true,
+              pattern: /[A-Za-z]{3}/,
+            })}
+            placeholder="Не указано"
+          />
+          <input
+            type="number"
+            {...methods.register('commissionPercent', {
+              required: true,
+              valueAsNumber: true,
+              pattern: /^-?[0-9]+(\.[0-9]+)?$/,
+            })}
+            placeholder="Не указано"
+          />
+          <input
+            type="number"
+            {...methods.register('commission', {
+              required: true,
+              valueAsNumber: true,
+              pattern: /^-?[0-9]+(\.[0-9]+)?$/,
+            })}
+            placeholder="Не указано"
+          />
+        </div>
+        <ApartamentFields />
+      </form>
+    </FormProvider>
+  );
+}
+
+function ApartamentFields() {
+  const { register } = useFormContext();
+
+  return (
+    <div className="grid grid-cols-3 gap-x-5">
+      <div className="flex flex-col gap-y-3">
+        <span className="font-semibold">Категория недвижимости</span>
+      </div>
+      <div className="flex flex-col gap-y-3">
+        <span className="font-semibold">Дом</span>
+      </div>
+      <div className="flex flex-col gap-y-3">
+        <span className="font-semibold">Территория / Особые условия</span>
+      </div>
+    </div>
   );
 }
